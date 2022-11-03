7cf7de43aec84cc3c41d2d3b6b7875572553b240
Bong0228 <karavtsev01@mail.ru>
2022-11-03 16:03:21 +0700

    first

---
+++ password.js
@@ -0,0 +1,6 @@
+console.log("Первое задание")
+let pass = prompt("Введите пароль:" )
+if ( pass.length > 4 && ( pass.includes('_') || pass.includes('-') ) )
+{console.log("Сильный пароль")}
+else
+{console.log("Слабый пароль")}
